a74db738d00e959fe74fbcd4d07256028b17f7f1
Clement Decou <decou.clement@gmail.com>
2017-12-31 20:08:32 +0100

    fix: fix error setup

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ module.exports = function (sails) {
 
     var setup = require('./lib/bose.setup.js');
     var init = require('./lib/bose.init.js');
-    var music = require('./lib/music/index.js');
+    //var music = require('./lib/music/index.js');
 
     gladys.on('ready', function(){
         init();
@@ -11,7 +11,7 @@ module.exports = function (sails) {
       
     return {
         setup, 
-        init,
+        init
         //music
     };
 };
