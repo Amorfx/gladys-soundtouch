5fa4297434093630bf6ece5bed9b4847c32f5ea5
Clement Decou <decou.clement@gmail.com>
2018-01-01 11:32:32 +0100

    feat: add abstract function for gladys music

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-
 module.exports = function (sails) {
 
     var setup = require('./lib/bose.setup.js');
     var init = require('./lib/bose.init.js');
-    //var music = require('./lib/music/index.js');
+    var music = require('./lib/music/index.js');
 
     gladys.on('ready', function(){
         init();
@@ -12,6 +11,6 @@ module.exports = function (sails) {
     return {
         setup, 
         init
-        //music
+        music
     };
 };
