43b9ad51f7657cd21035e597ed0541a75eb60002
Clement Decou <decou.clement@gmail.com>
2018-01-01 13:45:15 +0100

    feat: add exec to set volume

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function (sails) {
     var setup = require('./lib/bose.setup.js');
     var init = require('./lib/bose.init.js');
     var music = require('./lib/music/index.js');
+    var exec = require('./lib/exec.js')
 
     gladys.on('ready', function(){
         init();
@@ -11,6 +12,7 @@ module.exports = function (sails) {
     return {
         setup, 
         init,
-        music
+        music,
+        exec
     };
 };
