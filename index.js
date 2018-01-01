26bad1e836e5a32907d46ccd8479c5e4fd41830f
Clement Decou <decou.clement@gmail.com>
2018-01-01 13:52:41 +0100

    feat: remove exec

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ module.exports = function (sails) {
     var setup = require('./lib/bose.setup.js');
     var init = require('./lib/bose.init.js');
     var music = require('./lib/music/index.js');
-    var exec = require('./lib/exec.js')
 
     gladys.on('ready', function(){
         init();
@@ -12,7 +11,6 @@ module.exports = function (sails) {
     return {
         setup, 
         init,
-        music,
-        exec
+        music
     };
 };
